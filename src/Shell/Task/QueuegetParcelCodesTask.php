6477f0c6543c838b1f6c2d21cbc9a41ecd20565c
SK <sylwester.kowal@21w.pl>
2017-01-31 10:56:43 +0100

    Zapis numeru w Delivery.21order

--- src/Shell/Task/QueuegetParcelCodesTask.php
+++ src/Shell/Task/QueuegetParcelCodesTask.php
@@ -15,6 +15,8 @@ use Gls\Controller\Component\GlsComponent;
 use Import\Controller\Component\ImportServiceComponent;
 use Queue\Shell\Task\QueueTask;
 use Cake\Log\Log;
+use Cake\ORM\TableRegistry;
+use Cake\Datasource\ConnectionManager;
 
 /**
  * Class QueueImportProductTo21OrderTask
@@ -26,6 +28,7 @@ class QueuegetParcelCodesTask extends QueueTask
     private $orderId;
     private $deliveryId;
     private $delivery;
+    private $connection;
 
     public function setOrderId($orderId)
     {
@@ -51,43 +54,52 @@ class QueuegetParcelCodesTask extends QueueTask
 
     public function run(array $data, $id)
     {
-        $this->glsComponent = new GlsComponent(new ComponentRegistry());
+
         try {
-            $this->delivery = $this->glsComponent->init($data)->getDeliveryData($data);
-            if ($this->delivery /*&& $importProduct->ststus != 1*/) { // istnieje oraz nie był jeszcze importowany
 
-                $this->out($data['order_id']);
+            $this->connection = ConnectionManager::get('default');
 
-                $result = $this->glsComponent
-                    ->init($data)
-                    ->connect()
-                    ->checkParcelIsSetForOrder()
-                    ->saveParcelInStore();
+            $this->glsComponent = new GlsComponent(new ComponentRegistry());
 
-                $this->glsComponent->disconect();
+            $this->deliveryId = $data['delivery_id'];
+            $this->orderId = $data['order_id'];
 
-                if (isset($result['pn']) && !empty($result['pn'])) {
-                    $this->setTrackParcelNumberInDeliveryData($result['pn']);
-                }
+            $this->out($data['order_id']);
 
-                if ($result['err'] == 0) {
-                    $this->out($result['pn']);
-                    $this->out($result['mess']);
+            $result = $this->glsComponent
+                ->init($data)
+                ->connect()
+                ->checkParcelIsSetForOrder()
+                ->saveParcelInStore();
+
+            if (isset($result['pn']) && !empty($result['pn'])) {
+                $this->setTrackParcelNumberInDeliveryData($result['pn']);
+            }
+
+            $this->glsComponent->disconect();
+
+            if ($this->connection->isConnected()) {
+                $this->connection->disconnect();
+            }
+
+            if ($result['err'] == 0) {
+                $this->out($result['pn']);
+                $this->out($result['mess']);
 //                    $this->setStatus($data, ['status' => 1, 'errors' => '']);
-                    return true;
-                } else {
-                    $this->out($result['mess']);
+                return true;
+            } else {
+                $this->out($result['mess']);
 //                    $this->hr();
 //                    $this->setStatus($data, ['status' => 9, 'errors' => $err]);
-                    return false;
-                }
-
-
+                return false;
             }
+
+
         } catch (\Exception $e) {
             $this->log($e->getMessage());
             return false;
         }
+
 
         return true;
     }
@@ -96,30 +108,26 @@ class QueuegetParcelCodesTask extends QueueTask
     /**
      * Zapisuje numer przewozowy w Modelu
      */
-    public function setTrackParcelNumberInDeliveryData($pn)
+    public
+    function setTrackParcelNumberInDeliveryData($pn)
     {
-//        $this->Deliveries = TableRegistry::get('Deliveries');
+        if (!$this->connection->isConnected()) {
+            $this->connection->connect();
+        }
+        $this->Deliveries = TableRegistry::get('Deliveries');
+
+
+        $this->delivery = $this->Deliveries->find()
+            ->where([
+                'Deliveries.id' => $this->deliveryId,
+                'Deliveries.order_id' => $this->orderId])
+            ->contain('Stores')
+            ->first();
+
         $this->Deliveries->updateAll(
             ['parcel_number' => $pn],
             ['Deliveries.order_id' => $this->delivery->order_id, 'Deliveries.code' => $this->delivery->code]);
 
-    }
-
-
-    private function setStatus($data, $errors)
-    {
-        $importProduct = $this->getImportProduct($data);
-        $importProduct = $this->ImportProducts->patchEntity($importProduct, $errors);
-        if ($this->ImportProducts->save($importProduct)) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    private function setModels()
-    {
-        $this->loadModel('Deliveries');
     }
 
 }
